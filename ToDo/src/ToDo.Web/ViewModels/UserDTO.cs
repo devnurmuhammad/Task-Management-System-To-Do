9efb34d7696d21ccfc167f126b22f308823f6709
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-01-26 20:00:09 +0500

    user DTO added

---
+++ ToDo/src/ToDo.Web/ViewModels/UserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDo.Web.ViewModels
+{
+    public class UserDTO
+    {
+        public string UserName { get; set; } = default!;
+        public string Password { get; set; } = default!;
+    }
+}
