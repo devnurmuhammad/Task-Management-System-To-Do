77c48633473a404e150f19449511c4d68c2d8097
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-01-26 22:00:14 +0500

    viewModels moved to application layer

---
+++ ToDo/src/ToDo.Application/ViewModels/UserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDo.Application.ViewModels
+{
+    public class UserDTO
+    {
+        public string UserName { get; set; } = default!;
+        public string Password { get; set; } = default!;
+    }
+}
