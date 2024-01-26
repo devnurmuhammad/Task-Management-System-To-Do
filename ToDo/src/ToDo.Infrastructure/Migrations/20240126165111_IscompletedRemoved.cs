456724cea2885d2f5ad7a5c45d08125d111454b3
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-01-26 22:01:25 +0500

    taskController added

---
+++ ToDo/src/ToDo.Infrastructure/Migrations/20240126165111_IscompletedRemoved.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDo.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class IscompletedRemoved : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCompleted",
+                table: "Tasks");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCompleted",
+                table: "Tasks",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
