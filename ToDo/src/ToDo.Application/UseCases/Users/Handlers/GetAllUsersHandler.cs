f7a1b95e5219d28f6196bab5fdcb8d984611cdb8
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-01-26 19:59:12 +0500

    user handlers added

---
+++ ToDo/src/ToDo.Application/UseCases/Users/Handlers/GetAllUsersHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ToDo.Application.Abstractions;
+using ToDo.Application.UseCases.Users.Queries;
+using ToDo.Domain.Entities;
+
+namespace ToDo.Application.UseCases.Users.Handlers
+{
+    public class GetAllUsersHandler : IRequestHandler<GetAllUsersQuery, IList<User>>
+    {
+        private readonly IToDoApplicationDbContext _context;
+
+        public GetAllUsersHandler(IToDoApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IList<User>> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
+        {
+            IList<User> users = await _context.Users.Include(x => x.Tasks).ToListAsync(cancellationToken);
+            return users;
+        }
+    }
+}
