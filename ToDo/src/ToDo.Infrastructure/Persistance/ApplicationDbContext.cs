a70fe01fec2cfb5ca3a3c5d040db11d96abb013e
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-01-26 17:08:56 +0500

    DependencyInjection added to Infrastructure layer

---
+++ ToDo/src/ToDo.Infrastructure/Persistance/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo.Application.Abstractions;
+using ToDo.Domain.Entities;
+
+namespace ToDo.Infrastructure.Persistance
+{
+    public class ApplicationDbContext : DbContext, IToDoApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        
+        public DbSet<User> Users { get; set; }
+        public DbSet<TodoTask> Tasks { get; set; }
+    }
+}
