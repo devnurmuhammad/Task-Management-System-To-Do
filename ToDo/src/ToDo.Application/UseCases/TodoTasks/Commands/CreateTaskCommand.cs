925d47e3cd801831bf9f99c7190987a91f23f824
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-01-26 21:58:19 +0500

    task commands added

---
+++ ToDo/src/ToDo.Application/UseCases/TodoTasks/Commands/CreateTaskCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using ToDo.Application.ViewModels;
+
+namespace ToDo.Application.UseCases.TodoTasks.Commands
+{
+    public class CreateTaskCommand : IRequest
+    {
+        public TaskDTO TaskDto { get; set; }
+    }
+}
---
+++ ToDo/src/ToDo.Application/UseCases/TodoTasks/Commands/UpdateTaskCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using ToDo.Domain.Enums;
+
+namespace ToDo.Application.UseCases.TodoTasks.Commands
+{
+    public class UpdateTaskCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; } = default!;
+        public string? Definition { get; set; }
+        public DateTime Deadline { get; set; }
+        public IsProgress Progress { get; set; } = IsProgress.ToDo;
+    }
+}
