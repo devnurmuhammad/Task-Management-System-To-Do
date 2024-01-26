4c267c0828a01f93a83e059deeec459e9f63a397
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-01-26 17:07:05 +0500

    ToDoTask and User Entities created

---
+++ ToDo/src/ToDo.Domain/Entities/TodoTask.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using static ToDo.Domain.Enums.ProgressEnum;
+
+namespace ToDo.Domain.Entities
+{
+    public class TodoTask
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; } = default!;
+        public string? Definition { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime Deadline { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public IsProgress Progress { get; set; } = IsProgress.ToDo;
+        public int UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public User? Users { get; set; }
+    }
+}
