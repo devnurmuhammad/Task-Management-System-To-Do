f0f81f09f8c41e730b54e975323dfa5f85da950f
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-01-27 22:37:48 +0500

    progres default added

---
+++ ToDo/src/ToDo.Infrastructure/Migrations/20240127172038_userdelet.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDo.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class userdelet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Tasks");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Tasks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
