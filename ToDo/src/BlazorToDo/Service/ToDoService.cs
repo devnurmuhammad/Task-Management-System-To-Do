010977e21b772a381c87eb10c65b3716c6156b77
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-01-27 21:58:06 +0500

    todo service added

--- ToDo/src/BlazorToDo/Service/ToDoService.cs
+++ ToDo/src/BlazorToDo/Service/ToDoService.cs
@@ -14,9 +14,25 @@ namespace BlazorToDo.Service
         {
            tasks = await _httpClient.GetFromJsonAsync<List<TodoTask>>("http://localhost:5131/api/Task/GetAllTasks");
         }
+
+        public async Task<TodoTask> GetTaskById(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<TodoTask>($"http://localhost:5131/api/Task/GetTaskById?id={id}");
+
+        }
         public async Task DeleteTask(int id)
         {
             await _httpClient.DeleteAsync($"http://localhost:5131/api/Task/DeleteTask?id={id}");
+        }
+
+        public async Task AddTask(TodoTask newtask)
+        {
+            await _httpClient.PostAsJsonAsync("http://localhost:5131/api/Task/CreateTask", newtask);
+        }
+
+        public async Task UpdateTask(TodoTask editedtask)
+        {
+            await _httpClient.PutAsJsonAsync("http://localhost:5131/api/Task/UpdateTask", editedtask);
         }
     }
 }
