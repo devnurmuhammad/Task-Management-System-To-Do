cdc9a3c3eee6ca18f65fa73a3aad4745760e08c8
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-01-27 19:24:09 +0500

    entities added to blazor

---
+++ ToDo/src/BlazorToDo/Entities/TodoTask.cs
@@ -0,0 +1,18 @@
+﻿using BlazorToDo.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorToDo.Entities
+{
+    public class TodoTask
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; } = default!;
+        public string? Definition { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime Deadline { get; set; }
+        public IsProgress Progress { get; set; } = IsProgress.ToDo;
+        public int UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public User? Users { get; set; }
+    }
+}
